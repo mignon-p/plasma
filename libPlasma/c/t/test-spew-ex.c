40d23c58665ff632ca6609069e64dfaa8106452e
Mignon Pelletier <code@funwithsoftware.org>
2026-08-27 18:28:52 -0700

    Test SLAW_SPEW_FLAG_RUDE_ASCII.

--- libPlasma/c/t/test-spew-ex.c
+++ libPlasma/c/t/test-spew-ex.c
@@ -6,6 +6,7 @@
 #include "libLoam/c/ob-log.h"
 #include "libLoam/c/ob-vers.h"
 #include "libPlasma/c/slaw.h"
+#include "libPlasma/c/protein.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -213,6 +214,160 @@ static void test_relative_offset (void)
   slaw_free (s);
 }
 
+typedef struct accum_str
+{
+  char  *buf;
+  size_t len;
+  size_t capacity;
+} accum_str;
+
+static ob_retort accum_str_func (void       *cookie,
+                                 const char *str,
+                                 size_t      len)
+{
+  accum_str *acc   = (accum_str *) cookie;
+  size_t available = acc->capacity - acc->len;
+  size_t len1      = len + 1;   /* account for terminating NUL */
+
+  if (len1 >= available)
+    {
+      size_t newcap1 = acc->capacity * 2;
+      size_t newcap2 = acc->len + len1 + 128;
+      size_t newcap  = (newcap1 > newcap2) ? newcap1 : newcap2;
+      char  *newbuf  = realloc (acc->buf, newcap);
+
+      if (newbuf == NULL)
+        return OB_NO_MEM;
+
+      acc->buf      = newbuf;
+      acc->capacity = newcap;
+    }
+
+  memcpy (acc->buf + acc->len, str, len1);
+  acc->len += len;
+
+  return OB_OK;
+}
+
+static char *accum_spew_str (bslaw s,
+                             unt32 flags,
+                             const char *prolo)
+{
+  accum_str acc;
+  acc.len      = 0;
+  acc.capacity = 128;
+  acc.buf      = calloc (acc.capacity, sizeof (char));
+
+  if (acc.buf == NULL)
+    {
+      OB_FATAL_ERROR_CODE (0x2031b00f, "Out of memory");
+    }
+
+  ob_retort tort = slaw_spew_overview_to_func (s,
+                                               accum_str_func,
+                                               &acc,
+                                               80,
+                                               flags,
+                                               prolo);
+
+  OB_DIE_ON_ERR_CODE (0x2031b010, tort);
+
+  return acc.buf;
+}
+
+static slaw make_rude_protein (void)
+{
+  unt8 rude[256];
+  size_t i;
+
+  for (i = 0; i < sizeof (rude); i++)
+    rude[i] = (unt8) i;
+
+  return protein_from_ffr (slaw_nil(),
+                           slaw_nil(),
+                           rude, sizeof (rude));
+}
+
+static const char *const rude_ascii_expected_lines[] =
+  {
+    "> slaw[36o.0x000]: PROT: ((",
+    "> descrips:",
+    "> slaw[1o.0x010]: NIL.",
+    "> ingests:",
+    "> slaw[1o.0x018]: NIL.",
+    "> rude data: 256 bytes",
+    ">  00 01 02 03 04 05 06 07  08 09 0a 0b 0c 0d 0e 0f  |................|",
+    ">  10 11 12 13 14 15 16 17  18 19 1a 1b 1c 1d 1e 1f  |................|",
+    ">  20 21 22 23 24 25 26 27  28 29 2a 2b 2c 2d 2e 2f  | !\"#$%&'()*+,-./|",
+    ">  30 31 32 33 34 35 36 37  38 39 3a 3b 3c 3d 3e 3f  |0123456789:;<=>?|",
+    ">  40 41 42 43 44 45 46 47  48 49 4a 4b 4c 4d 4e 4f  |@ABCDEFGHIJKLMNO|",
+    ">  50 51 52 53 54 55 56 57  58 59 5a 5b 5c 5d 5e 5f  |PQRSTUVWXYZ[\\]^_|",
+    ">  60 61 62 63 64 65 66 67  68 69 6a 6b 6c 6d 6e 6f  |`abcdefghijklmno|",
+    ">  70 71 72 73 74 75 76 77  78 79 7a 7b 7c 7d 7e 7f  |pqrstuvwxyz{|}~.|",
+    ">  80 81 82 83 84 85 86 87  88 89 8a 8b 8c 8d 8e 8f  |................|",
+    ">  90 91 92 93 94 95 96 97  98 99 9a 9b 9c 9d 9e 9f  |................|",
+    ">  a0 a1 a2 a3 a4 a5 a6 a7  a8 a9 aa ab ac ad ae af  |................|",
+    ">  b0 b1 b2 b3 b4 b5 b6 b7  b8 b9 ba bb bc bd be bf  |................|",
+    ">  c0 c1 c2 c3 c4 c5 c6 c7  c8 c9 ca cb cc cd ce cf  |................|",
+    ">  d0 d1 d2 d3 d4 d5 d6 d7  d8 d9 da db dc dd de df  |................|",
+    ">  e0 e1 e2 e3 e4 e5 e6 e7  e8 e9 ea eb ec ed ee ef  |................|",
+    ">  f0 f1 f2 f3 f4 f5 f6 f7  f8 f9 fa fb fc fd fe ff  |................|",
+    ">  ))",
+    NULL
+  };
+
+static void test_rude_ascii (void)
+{
+  slaw  s   = make_rude_protein();
+  unt32 flg = SLAW_SPEW_FLAG_REL_OFF | SLAW_SPEW_FLAG_RUDE_ASCII;
+  char *str = accum_spew_str (s, flg, "> ");
+
+  diff_lines (str, rude_ascii_expected_lines);
+
+  free (str);
+  slaw_free (s);
+}
+
+static const char *const rude_noascii_expected_lines[] =
+  {
+    "% slaw[36o.0x000]: PROT: ((",
+    "% descrips:",
+    "% slaw[1o.0x010]: NIL.",
+    "% ingests:",
+    "% slaw[1o.0x018]: NIL.",
+    "% rude data: 256 bytes",
+    "%  00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f",
+    "%  10 11 12 13 14 15 16 17 18 19 1a 1b 1c 1d 1e 1f",
+    "%  20 21 22 23 24 25 26 27 28 29 2a 2b 2c 2d 2e 2f",
+    "%  30 31 32 33 34 35 36 37 38 39 3a 3b 3c 3d 3e 3f",
+    "%  40 41 42 43 44 45 46 47 48 49 4a 4b 4c 4d 4e 4f",
+    "%  50 51 52 53 54 55 56 57 58 59 5a 5b 5c 5d 5e 5f",
+    "%  60 61 62 63 64 65 66 67 68 69 6a 6b 6c 6d 6e 6f",
+    "%  70 71 72 73 74 75 76 77 78 79 7a 7b 7c 7d 7e 7f",
+    "%  80 81 82 83 84 85 86 87 88 89 8a 8b 8c 8d 8e 8f",
+    "%  90 91 92 93 94 95 96 97 98 99 9a 9b 9c 9d 9e 9f",
+    "%  a0 a1 a2 a3 a4 a5 a6 a7 a8 a9 aa ab ac ad ae af",
+    "%  b0 b1 b2 b3 b4 b5 b6 b7 b8 b9 ba bb bc bd be bf",
+    "%  c0 c1 c2 c3 c4 c5 c6 c7 c8 c9 ca cb cc cd ce cf",
+    "%  d0 d1 d2 d3 d4 d5 d6 d7 d8 d9 da db dc dd de df",
+    "%  e0 e1 e2 e3 e4 e5 e6 e7 e8 e9 ea eb ec ed ee ef",
+    "%  f0 f1 f2 f3 f4 f5 f6 f7 f8 f9 fa fb fc fd fe ff",
+    "%  ))",
+    NULL
+  };
+
+static void test_rude_noascii (void)
+{
+  slaw  s   = make_rude_protein();
+  unt32 flg = SLAW_SPEW_FLAG_REL_OFF;
+  char *str = accum_spew_str (s, flg, "% ");
+
+  diff_lines (str, rude_noascii_expected_lines);
+
+  free (str);
+  slaw_free (s);
+}
+
 int main (int argc, char **argv)
 {
   OB_DIE_ON_ERROR (OB_CHECK_ABI ());
@@ -225,6 +380,8 @@ int main (int argc, char **argv)
   test_null_slaw ();
   test_null_slaw_with_prolo ();
   test_relative_offset ();
+  test_rude_ascii ();
+  test_rude_noascii ();
 
   return EXIT_SUCCESS;
 }
