b18101314939d0c62d9d3e3de7a02b2c1a3f8870
Mignon Pelletier <code@funwithsoftware.org>
2026-08-29 18:04:29 -0700

    Allow specifying options to the Spew methods via SpewOptions.
    This exposes the "flags" and "prolo" capabilities of the C API.

--- libPlasma/c++/Slaw.cpp
+++ libPlasma/c++/Slaw.cpp
@@ -213,28 +213,113 @@ ob_retort spew_ostream_func (void *v, const char *str, size_t len)
   return OB_OK;
 }
 
+const unt32 DEFAULT_SPEW_FLAGS =
+  SLAW_SPEW_FLAG_REL_OFF       |
+  SLAW_SPEW_FLAG_RUDE_ASCII    |
+  SLAW_SPEW_FLAG_ESCAPE_STRINGS;
+
+const Slaw::SpewOptions DEFAULT_SPEW_OPTIONS;
+
 }
 
 void Slaw::Spew (OStreamReference os) const
+{
+  Spew (os, DEFAULT_SPEW_OPTIONS);
+}
+
+void Slaw::Spew (OStreamReference os, const SpewOptions &opts) const
 {
   slaw_spew_overview_to_func (SlawValue (),
                               spew_ostream_func,
                               &os,
                               0,
-                              0,
-                              NULL);
+                              opts.GetFlags (),
+                              opts.GetPrefix ());
 }
 
 void Slaw::Spew (FILE *ph) const
 {
+  Spew (ph, DEFAULT_SPEW_OPTIONS);
+}
+
+void Slaw::Spew (FILE *ph, const SpewOptions &opts) const
+{
   if (!ph)
     return;
-  slaw_spew_overview (SlawValue (), ph, NULL);
+  slaw_spew_overview_ex (SlawValue (),
+                         ph,
+                         opts.GetFlags (),
+                         opts.GetPrefix ());
 }
 
 void Slaw::SpewToStderr () const
 {
   Spew (::std::cerr);
+}
+
+Slaw::SpewOptions::SpewOptions () : flags_ (DEFAULT_SPEW_FLAGS)
+{
+}
+
+Slaw::SpewOptions::SpewOptions (unt32 flags) : flags_ (flags)
+{
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::ChangeFlag (unt32 flag, bool use)
+{
+  if (use)
+    return AddFlags (flag);
+  else
+    return RemoveFlags (flag);
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::RelativeOffset (bool use)
+{
+  return ChangeFlag (SLAW_SPEW_FLAG_REL_OFF, use);
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::RudeAscii (bool use)
+{
+  return ChangeFlag (SLAW_SPEW_FLAG_RUDE_ASCII, use);
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::EscapeStrings (bool use)
+{
+  return ChangeFlag (SLAW_SPEW_FLAG_ESCAPE_STRINGS, use);
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::AddFlags (unt32 flags)
+{
+  flags_ |= flags;
+  return *this;
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::RemoveFlags (unt32 flags)
+{
+  flags_ &= ~flags;
+  return *this;
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::SetFlags (unt32 flags)
+{
+  flags_ = flags;
+  return *this;
+}
+
+unt32 Slaw::SpewOptions::GetFlags () const
+{
+  return flags_;
+}
+
+Slaw::SpewOptions &Slaw::SpewOptions::Prefix (const Str& prolo)
+{
+  prolo_ = prolo;
+  return *this;
+}
+
+Str Slaw::SpewOptions::GetPrefix () const
+{
+  return prolo_;
 }
 
 
--- libPlasma/c++/tests/SlawTest.cpp
+++ libPlasma/c++/tests/SlawTest.cpp
@@ -161,7 +161,7 @@ TEST (SlawTest1, Spew)
 {
   Slaw s = Slaw::Map ("type", Str ("Blank"), "num", (int32) 96);
   std::ostringstream silly_string;
-  s.Spew (silly_string);
+  s.Spew (silly_string, Slaw::SpewOptions (0));
   Str actual (silly_string.str ().c_str ());
   Str expected ("slaw[xxx]: MAP (2 elems): {\n"
                 " 1: slaw[xxx]: CONS:\n"
@@ -172,6 +172,31 @@ TEST (SlawTest1, Spew)
                 " 2:  R: slaw[xxx]: INT32 = 96\n"
                 " }");
   actual.ReplaceAll ("slaw\\[\\d+[oq]\\.[0-9A-Fa-fxX]+\\]:", "slaw[xxx]:");
+  EXPECT_STREQ (expected, actual);
+}
+
+TEST (SlawTest1, SpewOptions)
+{
+  Slaw::SpewOptions opts;
+  opts.Prefix ("banana: ");
+
+  const char rude[] = "blech";
+  Slaw s (protein_from_ffr (slaw_nil(),
+                            slaw_nil(),
+                            rude, sizeof (rude)));
+
+  std::ostringstream silly_string;
+  s.Spew (silly_string, opts);
+
+  Str actual (silly_string.str ().c_str ());
+  Str expected ("banana: slaw[4o.0x00]: PROT: ((\n"
+                "banana: descrips:\n"
+                "banana: slaw[1o.0x10]: NIL.\n"
+                "banana: ingests:\n"
+                "banana: slaw[1o.0x18]: NIL.\n"
+                "banana: rude data: 6 bytes\n"
+                "banana:  62 6c 65 63 68 00                                 |blech.|\n"
+                "banana:  ))");
   EXPECT_STREQ (expected, actual);
 }
 
@@ -292,7 +317,7 @@ TEST (SlawTest1, Foreach)
   Slaw p (Slaw::List ("a", "b", "c"));
   for (const Slaw &descrip : p)
     {
-      descrip.Spew (os);
+      descrip.Spew (os, Slaw::SpewOptions (0));
       os << std::endl;
     }
   Str actual (os.str ().c_str ());
